8a23a2d79308041f35322bc809516ef059912ce8
chris martinex <uschrismartinez@gmail.com>
2023-03-21 15:59:07 -0700

    card made

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import { useEffect } from 'react';
 import './App.css';
 import React from 'react';
 import DataServices from './Data/DataServices';
 import ApiComponent from './Components/ApiComponent';
+import Header from './Components/Header';
+import WeatherCard from './Components/WeatherCard';
+import axios from 'axios';
 
+const URL = `https://api.openweathermap.org/data/3.0/onecall`;
+const API_KEY = `dd39ad3836cd95e0847faf228f7f4b17`;
 
 export default function App() {
-
+  useEffect(() => {
+    axios.get()
+  }, [])
   return (
-    <div>
+    <div className='main' >
+       <Header/>
+      <WeatherCard />
       {/* <DataServices /> */}
-      <ApiComponent />
+      {/* <ApiComponent /> */}
       {/* <functionComponent/> */}
       {/* <weatherComponent /> */}
     </div>
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Header () {
+
+ return(
+
+   <div className="App header">
+    React Weather Application
+   </div>
+
+ )
+
+}
